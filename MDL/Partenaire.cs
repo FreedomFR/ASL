85fa6f900c66588702d552b8933296532abb8f13
Mayol Boucaud <FreedomFR@users.noreply.github.com>
2019-12-05 16:11:23 +0100

    Ajoutez des fichiers projet.

---
+++ MDL/Partenaire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDL
+{
+    class Partenaire
+    {
+        private Boolean cluborganisateur;
+        private Boolean equipementier;
+
+        public Partenaire(Boolean unClub, Boolean unEquip)
+        {
+            this.cluborganisateur = unClub;
+            this.equipementier = unEquip;
+        }
+
+    }
+
+}
---
+++ MDL/Stand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDL
+{
+    class Stand
+    {
+        private int alle;
+        private int ordre;
+
+        public Stand(int uneAlle, int unOrdre)
+        {
+            this.alle = uneAlle;
+            this.ordre = unOrdre;
+        }
+
+
+    }
+}
